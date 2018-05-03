c4830b1f3f1f73d9782905621484985ea3e3fc74
umutluoglu <36135666+umutluoglu@users.noreply.github.com>
2018-05-04 00:43:43 +0300

    Update dotcfp.php

--- config/dotcfp.php
+++ config/dotcfp.php
@@ -1,28 +1,28 @@
 <?php
 
 return [
-    'event_name' => 'PHPKonf',
+    'event_name' => 'Dotnet Konferansı',
 
     //Promo site URL (Not dotCFP!)
-    'event_site' => 'http://phpkonf.org/',
+    'event_site' => 'http://devnot.com/',
 
     //Event Contact email
-    'event_email' => 'mail@istanbulphp.org',
+    'event_email' => 'ugur@devnot.com',
 
     //Timezone
     'timezone' => 'Europe/Istanbul',
 
     //Event start date (Y-m-d)
-    'start_date' => '2018-05-20',
+    'start_date' => '2018-10-13',
 
     //Event end date (Y-m-d)
-    'end_date' => '2018-05-20',
+    'end_date' => '2018-10-13',
 
     //Submit talk feature opened after the date below (Y-m-d)
-    'cfp_start_date' => '2017-11-20',
+    'cfp_start_date' => '2018-05-03',
 
     //Submit talk feature closed after the date below (Y-m-d)
-    'cfp_end_date' => '2018-01-31',
+    'cfp_end_date' => '2018-06-30',
 
     //Nearby airport of event location. Eg: Amsterdam -> AMS
     'airport_code' => 'IST',
@@ -31,14 +31,11 @@ return [
     'maps' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d770809.0860158126!2d28.45174621544522!3d41.003964328513874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa7040068086b%3A0xe1ccfe98bc01b0d0!2zxLBzdGFuYnVs!5e0!3m2!1str!2str!4v1511099923825" width="330" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
 
     'speaker_packages' => [
-        '2 complimentary hotel nights',
+        '1 complimentary hotel nights',
         'Complimentary airfare/travel'
     ],
 
     'previous_years' => [
-        'http://2017.phpkonf.org',
-        'http://2016.phpkonf.org',
-        'http://2015.phpkonf.org',
-        'http://2014.phpkonf.org',
+
     ]
 ];
